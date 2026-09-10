e981c2f496bdb96b3beca0075cc1fd3bca6d54d4
kitsengyu-web <kitsengyu@gmail.com>
2026-09-10 13:49:28 +0530

    Update auth-button.tsx

--- components/auth-button.tsx
+++ components/auth-button.tsx
@@ -12,16 +12,24 @@ export async function AuthButton() {
   const user = data?.claims;
 
   return user ? (
-    <div className="flex items-center gap-4">
+    <div className="flex items-center gap-4 text-white">
       Hey, {user.email}!
       <LogoutButton />
     </div>
   ) : (
     <div className="flex gap-2">
-      <Button asChild size="sm" variant={"outline"}>
+      <Button
+        asChild
+        size="sm"
+        className="bg-zinc-900 text-white border border-zinc-700 hover:bg-zinc-800 hover:text-white"
+      >
         <Link href="/auth/login">Sign in</Link>
       </Button>
-      <Button asChild size="sm" variant={"default"}>
+      <Button
+        asChild
+        size="sm"
+        className="bg-black text-white border border-zinc-600 hover:bg-zinc-900 hover:text-white"
+      >
         <Link href="/auth/sign-up">Sign up</Link>
       </Button>
     </div>
