7983036eda5a6bea8e0972877d00668a1983ea39
kitsengyu-web <kitsengyu@gmail.com>
2026-09-10 23:11:43 +0530

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -16,7 +16,7 @@ const PramaanLogoIcon = () => (
     <path d="M43.5 138.8L59.3 46.2C60.5 39 67.2 34 74.5 35.2L124.6 43.6C131.8 44.8 136.8 51.5 135.6 58.7L129.8 92.6C128.6 99.8 121.9 104.8 114.7 103.6L86.4 98.9L79.1 141.2C77.9 148.4 71.2 153.4 63.9 152.2L49.5 149.8C42.3 148.6 37.3 141.9 38.5 134.7L43.5 138.8Z" />
   </svg>
 );
-
+const containerRef = useRef(null);
 export default function Home() {
   return (
     <main className="relative min-h-screen flex flex-col items-center overflow-hidden bg-black text-white">
