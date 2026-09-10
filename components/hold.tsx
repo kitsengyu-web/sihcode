f7648a9efa0090900a17c484e53d917a86cc9af8
kitsengyu-web <kitsengyu@gmail.com>
2026-09-11 00:06:33 +0530

    Update hold.tsx

--- components/hold.tsx
+++ components/hold.tsx
@@ -12,7 +12,7 @@ export function HeroSection() {
       className="relative z-10 my-auto flex items-center justify-center p-8 text-center"
     >
       <VariableProximity
-        label={"Hover me! And then star React Bits on GitHub, or else..."}
+        label={"The Solution for Procurement of Official Standards details are here"}
         className={"variable-proximity-demo text-2xl md:text-4xl font-bold cursor-pointer"}
         fromFontVariationSettings="'wght' 400, 'opsz' 9"
         toFontVariationSettings="'wght' 1000, 'opsz' 40"
