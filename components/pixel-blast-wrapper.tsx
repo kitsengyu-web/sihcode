7682b29295acbc17b7c825e32383799d424795a8
kitsengyu-web <kitsengyu@gmail.com>
2026-09-10 13:38:02 +0530

    Update pixel-blast-wrapper.tsx

--- components/pixel-blast-wrapper.tsx
+++ components/pixel-blast-wrapper.tsx
@@ -2,7 +2,7 @@
 
 import dynamic from "next/dynamic";
 
-const PixelBlast = dynamic(() => import("./PixelBlast"), {
+const PixelBlast = dynamic(() => import("@/components/landground"), {
   ssr: false,
 });
 
